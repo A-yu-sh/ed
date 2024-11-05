95e1690e61e99f159c16b6158230b5bd91287a9e
A-yu-sh <ayush.wd.dev@gmail.com>
2024-11-05 23:45:17 +0530

    header course updates

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,33 +1,53 @@
 import React from "react";
+import MBA from "../assets/Course logo/MBA Logo.png";
+import MCOM from "../assets/Course logo/M Com logo.png";
+import MCA from "../assets/Course logo/MCA logo.png";
+import MA from "../assets/Course logo/MA.png";
+import MSC from "../assets/Course logo/MSC.png";
+import MJMC from "../assets/Course logo/MJMC.png";
 
 function Course() {
   const courses = [
     {
       id: 1,
-      title: "MBA",
       subtitle: "Master of Business Administration",
       duration: "2 Year",
+      image: MBA,
     },
-    { id: 2, title: "MCOM", subtitle: "Master of Commerce" },
-    { id: 3, title: "MCA", subtitle: "Master of Computer Application" },
-    { id: 4, title: "MA", subtitle: "Master of Arts" },
-    { id: 5, title: "MSC", subtitle: "Master of Science" },
+    {
+      id: 2,
+      subtitle: "Master of Commerce",
+      image: MCOM,
+    },
+    {
+      id: 3,
+      subtitle: "Master of Computer Application",
+      image: MCA,
+    },
+    {
+      id: 4,
+      subtitle: "Master of Arts",
+      image: MA,
+    },
+    {
+      id: 5,
+      subtitle: "Master of Science",
+      image: MSC,
+    },
     {
       id: 6,
-      title: "MJMC",
       subtitle: "Master of Journalism & Mass Media Communications",
+      image: MJMC,
     },
   ];
 
   return (
     <div className="c-container">
-      <div className="courses-courses-container">
-        {" "}
-        <div className="courses-container">
+      <div className="univ-courses-container">
+        <div className="univ-container">
           {/* Sidebar */}
           <div className="sidebar">
-            <div class="online-courses-header">Online Courses</div>
-
+            <div className="online-courses-header">Online Courses</div>
             <p>Distance Courses</p>
             <p>
               Master Courses{" "}
@@ -47,13 +67,16 @@ function Course() {
             </p>
             <p>Certificate Courses</p>
           </div>
-
           {/* Courses grid */}
           <div className="courses-grid">
             {courses.map((course) => (
               <div key={course.id} className="course-card">
-                <h2>{course.title}</h2>
-                <p>{course.subtitle}</p>
+                <div className="course-title">
+                  <img className="course-icon" src={course.image} />
+                  <h2>{course.title}</h2>
+                </div>
+                <p className="course-subtitle">{course.subtitle}</p>{" "}
+                {/* Subtitle class applied */}
                 <div className="course-actions">
                   <button className="explore-btn">Explore</button>
                   <button className="view-btn">View Universities</button>

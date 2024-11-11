f399fbff457c7df80bd03753c2c4939ba72e30d8
A-yu-sh <ayush.wd.dev@gmail.com>
2024-11-11 20:13:02 +0530

    slider changes

--- src/components/AutomaticSlideShow.jsx
+++ src/components/AutomaticSlideShow.jsx
@@ -112,7 +112,7 @@ const LogoSlider = () => {
   const style = {
     // Main container styles
     sliderContainer: {
-      maxWidth: "100vw",
+      maxWidth: "80vw",
       overflow: "hidden",
       position: "relative",
       padding: "5px 0",

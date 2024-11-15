53784842a8edcbd41f78c811a17d42023c12fc01
A-yu-sh <ayush.wd.dev@gmail.com>
2024-11-16 02:34:32 +0530

    changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -217,7 +217,7 @@ const Form = () => {
           margin-top: 0vh;
           margin-bottom: 10px;
         }
-        @media (max-width: 1080px) {
+        @media (max-width: 1680px) {
           .enquiry-header {
             text-align: center;
             margin-top: 10vh;

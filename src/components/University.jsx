fbdb0ef2405e6d9815e7cc9c4168801dd2e400b0
A-yu-sh <ayush.wd.dev@gmail.com>
2024-11-20 12:24:57 +0530

    header image updates

--- src/components/University.jsx
+++ src/components/University.jsx
@@ -5,6 +5,24 @@ import JMIimage from "../assets/GOVT logo/jamia-millia-islamia-logo-hd-png-vecto
 import AUimage from "../assets/GOVT logo/Andhra University.jpg";
 import MCUimage from "../assets/GOVT logo/Mizoram_University_logo.png";
 import Hlogo from "../assets/GOVT logo/University_of_Hyderabad_Logo.png";
+import pv1 from "../assets/Goverment University image/Private university logo/Amity.jpg";
+import pv2 from "../assets/Goverment University image/Private university logo/Amrita.jpg";
+import pv3 from "../assets/Goverment University image/Private university logo/Chandigarh.jpg";
+import pv4 from "../assets/Goverment University image/Private university logo/DPU.png";
+import pv5 from "../assets/Goverment University image/Private university logo/Jain.jpg";
+import pv6 from "../assets/Goverment University image/Private university logo/Lovely_Professional_University_logo.png";
+import pv7 from "../assets/Goverment University image/Private university logo/Mangalayatan.jpeg";
+import pv8 from "../assets/Goverment University image/Private university logo/Manipal.png";
+import pv9 from "../assets/Goverment University image/Private university logo/SMU manipal.png";
+import pv10 from "../assets/Goverment University image/Private university logo/Sharda_University_logo_no_wordmark.png";
+import pv11 from "../assets/Goverment University image/Private university logo/Suresh Gyan Vihar university.jpg";
+import pv12 from "../assets/Goverment University image/Private university logo/UPES.jpg";
+import pv13 from "../assets/Goverment University image/Private university logo/Uttaranchal.png";
+import pv14 from "../assets/Goverment University image/Private university logo/Vignan.jpeg";
+import pv15 from "../assets/Goverment University image/Private university logo/Vivekananda global.png";
+import pv16 from "../assets/Goverment University image/Private university logo/shobhit university.png";
+import pv17 from "../assets/Goverment University image/Private university logo/shoolini.png";
+import pv18 from "../assets/Goverment University image/Private university logo/swami vivekananda university.jpeg";
 import { Link } from "react-router-dom";
 
 function Course() {
@@ -36,53 +54,53 @@ function Course() {
       image: Hlogo,
       clicked: "/filter",
     },
-    { id: 8, title: "Amrita University", image: ANimage, clicked: "/filter" },
-    { id: 9, title: "Jain University", image: AMimage, clicked: "/filter" },
+    { id: 8, title: "Amrita University", image: pv2, clicked: "/filter" },
+    { id: 9, title: "Jain University", image: pv5, clicked: "/filter" },
     {
       id: 10,
       title: "Chandigarh University",
-      image: AUimage,
+      image: pv3,
       clicked: "/filter",
     },
-    { id: 11, title: "DPU University", image: JMIimage, clicked: "/filter" },
-    { id: 12, title: "Amity University", image: MCUimage },
+    { id: 11, title: "DPU University", image: pv4, clicked: "/filter" },
+    { id: 12, title: "Amity University", image: pv1 },
     {
       id: 13,
       title: "Mangalayatan University",
-      image: ANimage,
+      image: pv7,
       clicked: "/filter",
     },
-    { id: 14, title: "Manipal College", image: Hlogo, clicked: "/filter" },
-    { id: 15, title: "SGVU University", image: AUimage, clicked: "/filter" },
-    { id: 16, title: "Sharda University", image: JMIimage, clicked: "/filter" },
+    { id: 14, title: "Manipal College", image: pv8, clicked: "/filter" },
+    { id: 15, title: "SGVU University", image: pv11, clicked: "/filter" },
+    { id: 16, title: "Sharda University", image: pv10, clicked: "/filter" },
     {
       id: 17,
       title: "Shobhit University",
-      image: MCUimage,
+      image: pv16,
       clicked: "/filter",
     },
-    { id: 18, title: "SMU University", image: ANimage, clicked: "/filter" },
+    { id: 18, title: "SMU University", image: pv9, clicked: "/filter" },
   ];
 
   const Distance_University = [
     {
       id: 19,
       title: "Lovely Professional University",
-      image: ANimage,
+      image: pv6,
       clicked: "/filter",
     },
-    { id: 20, title: "Shohbit University", image: AMimage, clicked: "/filter" },
+    { id: 20, title: "Shohbit University", image: pv16, clicked: "/filter" },
     {
       id: 21,
       title: "Mangalayatan University",
-      image: AUimage,
+      image: pv7,
       clicked: "/filter",
     },
-    { id: 22, title: "Suresh Gyan Vihar", image: JMIimage, clicked: "/filter" },
+    { id: 22, title: "Suresh Gyan Vihar", image: pv11, clicked: "/filter" },
     {
       id: 23,
       title: "Swami Vivekananda University",
-      image: MCUimage,
+      image: pv18,
       clicked: "/filter",
     },
   ];

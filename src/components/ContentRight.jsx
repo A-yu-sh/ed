15f39f1390ad900c0cc4c539ef449ac802a6db37
A-yu-sh <ayush.wd.dev@gmail.com>
2024-11-28 22:01:51 +0530

    space changes

--- src/components/ContentRight.jsx
+++ src/components/ContentRight.jsx
@@ -135,7 +135,7 @@ function ContentRight() {
               alt="Top 10 MBA Universities"
               style={styles.iconImage}
             />
-            <p style={styles.text}>Top 10 Distance University</p>
+            <p style={styles.text}>Top &nbsp, 10 Distance &nbsp, University</p>
           </div>
           <div style={styles.iconBox}>
             <img
@@ -151,7 +151,7 @@ function ContentRight() {
               alt="Top 10 MCA Universities"
               style={styles.iconImage}
             />
-            <p style={styles.text}>Top 10 Online Universities</p>
+            <p style={styles.text}>Top &nbsp, 10 Online &nbsp, Universities</p>
           </div>
           <div style={styles.iconBox}>
             <img
